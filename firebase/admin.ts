d61dea85dbd1e6d0a5e703821517d2774a92b68f
ShreeshaHegde09 <shreeshahegde09@gmail.com>
2025-07-15 18:16:09 +0530

    some errors resolved of privatekeys (the app is built properlyy)

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,18 +1,21 @@
-import { initializeApp, getApps, cert } from 'firebase-admin/app'
-import { getAuth } from 'firebase-admin/auth'
-import { getFirestore } from 'firebase-admin/firestore'
+// admin.ts
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 
 const initFirebaseAdmin = () => {
-    const apps =getApps();
+  const apps = getApps();
 
-    if(!apps.length){
-        initializeApp({
-            credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!))
-        })
-    }
-    return {
-        auth: getAuth(),
-        db: getFirestore()
-    }
-}
+  if (!apps.length) {
+    initializeApp({
+      credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!))
+    });
+  }
+
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
+};
+
 export const { auth, db } = initFirebaseAdmin();
