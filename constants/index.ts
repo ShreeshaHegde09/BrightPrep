eb946fab84e5798fc892e12ee2f3b0637982a8f9
ShreeshaHegde09 <shreeshahegde09@gmail.com>
2025-08-01 19:35:24 +0530

    changed vapi settings

--- constants/index.ts
+++ constants/index.ts
@@ -116,8 +116,8 @@ export const interviewer: CreateAssistantDTO = {
     useSpeakerBoost: true,
   },
   model: {
-    provider: "openai",
-    model: "gpt-4",
+    provider: "google",
+    model: "gemini-2.5-pro",
     messages: [
       {
         role: "system",
